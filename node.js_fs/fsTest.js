dcd6a9c33aed1dc020268f9386e81289899a9864
emacslisp <wudi850620@hotmail.com>
2017-02-03 09:13:52 +1100

    update node.js fs

--- node.js_fs/fsTest.js
+++ node.js_fs/fsTest.js
@@ -1,4 +1,4 @@
-//@example: node.js fs - mkdirp function
+//@example: node.js fs - mkdirp function with sync and 0777
 const fs = require('fs');
 var mkdirp = require('mkdirp');
 
@@ -7,9 +7,16 @@ var target_dir = '/var/www/odin/public/';
 var oldmask = process.umask(0);
 if(!fs.existsSync(target_dir)) {
     //console.log('Attempting to make dir');
-    mkdirp(target_dir, '0777', function(err){
-        process.umask(oldmask);        
-        if(err) console.error(err);
-        else console.log('dir created');
-    });
+    mkdirp.sync(target_dir, '0777', null);
+    process.umask(oldmask);        
 }
+
+var target_file = target_dir + 'fabhl.js';
+
+var ws = fs.createWriteStream(target_file, { flags : 'w' });
+ws.on('end', function() {
+    //console.log('Write Completed');
+});
+ws.on('close', function() {
+    console.log('file close');
+});
