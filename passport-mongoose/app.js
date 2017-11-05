d246bf0358f2ec80ea0faef6d693aeaf28d6458f
emacslisp <java.gdb@gmail.com>
2017-11-06 09:03:22 +1100

    add some new files

---
+++ passport-mongoose/app.js
@@ -0,0 +1,16 @@
+// main config
+var app = express();
+app.set('port', process.env.PORT || 1337);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', { layout: false });
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
