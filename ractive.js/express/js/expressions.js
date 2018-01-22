de237928a0950a2f97518460698fca78d0f52c54
emacslisp <java.gdb@gmail.com>
2018-01-23 10:43:00 +1100

    update expressoin and expression

--- ractive.js/express/js/expressions.js
+++ ractive.js/express/js/expressions.js
@@ -5,6 +5,8 @@ var ractive = new Ractive({
     city: 'Copenhagen',
     population: 763908,
     inCopenhagen: 601448,
+      colors:['red','green','blue'],
+      color: 'green',
     format: function ( num ) {
       if ( num > 1000 ) return ( Math.floor( num / 1000 ) ) + ',' + ( num % 1000 );
       return num;
